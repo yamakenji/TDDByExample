a22bec8185aaeb254c96f66f83ac05ac275fabf4
Kenji <yamauchi_ke@mti.co.jp>
2021-03-18 17:02:01 +0900

    Add: Dollar.java , MoneyTest.java

---
+++ src/main/java/jp/goodenough/tdd/money/Dollar.java
@@ -0,0 +1,12 @@
+package jp.goodenough.tdd.money;
+
+public class Dollar {
+    int amount;
+    Dollar(int amount) {
+
+    }
+
+    void times(int multiplier) {
+
+    }
+}
---
+++ src/test/java/jp/goodenough/tdd/money/MoneyTest.java
@@ -0,0 +1,14 @@
+package jp.goodenough.tdd.money;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MoneyTest {
+    @Test
+    public void testMultiplication() {
+        Dollar five = new Dollar(5);
+        five.times(2);
+        assertEquals(10, five.amount);
+    }
+}
